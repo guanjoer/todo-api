51e1b52831f3307cfa67d52146d700f623a99b1f
Youngmin Jo <54759844+designeryoungmin@users.noreply.github.com>
2024-06-21 23:13:34 +0900

    Fix: Update Middlewares

--- middlewares/enable-cors.js
+++ middlewares/enable-cors.js
@@ -1,9 +1,13 @@
 function enableCors(req, res, next) {
 	res.setHeader('Access-Control-Allow-Origin', '*'); // 리소스에 접근을 허용 하고자 페이지
 	res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE,OPTIONS') // 허용 하고자 하는 HTTP Requests
-	res.setHeader('Access-Control-Allow-Headers', 'Content-Type') // 허용하고자 하는 Headers
-
-	next()
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // 허용하고자 하는 Headers
+    
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200); // OPTIONS 요청에 대해 바로 200 응답을 반환
+    } else {
+        next();
+    }
 }
 
 
